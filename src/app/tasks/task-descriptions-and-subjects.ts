1a1de2e1462faf7cdbde6b91ecbd696bb916c057
jakubbedra <jakubbedra@wp.pl>
2022-05-20 15:24:27 +0200

    code change

--- src/app/tasks/task-descriptions-and-subjects.ts
+++ src/app/tasks/task-descriptions-and-subjects.ts
@@ -3,11 +3,13 @@ export class TaskDescriptionsAndSubjects {
 
   public static SUBJECTS: Record<string, string> = {
     BFS: "Breadth-First Search",
+    DFS: "Depth-First Search",
     COMPLETE_GRAPHS: "Complete Graphs"
   };
 
   public static DESCRIPTIONS: Record<string, string> = {
     BFS_VERTEX_SELECTION: "Select all vertices in the order they are visited using the BFS algorithm.",
+    DFS_VERTEX_SELECTION: "Select all vertices in the order they are visited using the DFS algorithm.",
     COMPLETE_GRAPHS_DRAW: "Draw the following graph: K {}"
   };
 
