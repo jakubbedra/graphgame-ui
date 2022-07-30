3b7f8873ad999dd0dff68d2c09be5c083233e078
jakubbedra <jakubbedra@wp.pl>
2022-07-30 11:58:31 +0200

    added 4 new tasks: - drawing star graphs - drawing path graphs - drawing wheel graphs - drawing cycle graphs

--- src/app/tasks/task-descriptions-and-subjects.ts
+++ src/app/tasks/task-descriptions-and-subjects.ts
@@ -4,13 +4,21 @@ export class TaskDescriptionsAndSubjects {
   public static SUBJECTS: Record<string, string> = {
     BFS: "Breadth-First Search",
     DFS: "Depth-First Search",
-    COMPLETE_GRAPHS: "Complete Graphs"
+    COMPLETE_GRAPHS: "Complete Graphs",
+    PATH_GRAPHS: "Path Graphs",
+    CYCLE_GRAPHS: "Cycle Graphs",
+    STAR_GRAPHS: "Star Graphs",
+    WHEEL_GRAPHS: "Wheel Graphs"
   };
 
   public static DESCRIPTIONS: Record<string, string> = {
     BFS_VERTEX_SELECTION: "Select all vertices in the order they are visited using the BFS algorithm.",
     DFS_VERTEX_SELECTION: "Select all vertices in the order they are visited using the DFS algorithm.",
-    COMPLETE_GRAPHS_DRAW: "Draw the following graph: K {}"
+    COMPLETE_GRAPHS_DRAW: "Draw the following graph: K {}",
+    PATH_GRAPHS_DRAW: "Draw the following graph: P {}",
+    CYCLE_GRAPHS_DRAW: "Draw the following graph: C {}",
+    STAR_GRAPHS_DRAW: "Draw the following graph: S {}",
+    WHEEL_GRAPHS_DRAW: "Draw the following graph: W {}"
   };
 
 }
