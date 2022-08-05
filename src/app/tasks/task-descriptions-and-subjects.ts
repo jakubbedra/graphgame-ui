efa82c993c03efa45eb4c2c510cd9a461b5e7126
jakubbedra <jakubbedra@wp.pl>
2022-08-05 17:16:36 +0200

    Added min vertex cover task.

--- src/app/tasks/task-descriptions-and-subjects.ts
+++ src/app/tasks/task-descriptions-and-subjects.ts
@@ -12,14 +12,16 @@ export class TaskDescriptionsAndSubjects {
     HYPERCUBES: "Hypercubes",
     REGULAR_GRAPHS: "Regular Graphs",
     MAX_CLIQUE: "Max Clique Problem",
-    MAX_INDEPENDENT_SET: "Max Independent Set Problem"
+    MAX_INDEPENDENT_SET: "Max Independent Set Problem",
+    MIN_VERTEX_COVER: "Min Vertex Cover"
   };
 
   public static DESCRIPTIONS: Record<string, string> = {
     BFS_VERTEX_SELECTION: "Select all vertices in the order they are visited using the BFS algorithm.",
     DFS_VERTEX_SELECTION: "Select all vertices in the order they are visited using the DFS algorithm.",
-    MAX_CLIQUE_VERTEX_SELECTION: "Select vertices which make the max clique (ore one of the max cliques) in the following graph.",
-    MAX_INDEPENDENT_SET_VERTEX_SELECTION: "Select vertices which make the max independent set (ore one of the independent sets) in the following graph.",
+    MAX_CLIQUE_VERTEX_SELECTION: "Select vertices which make the max clique (ore one of them) in the following graph.",
+    MAX_INDEPENDENT_SET_VERTEX_SELECTION: "Select vertices which make the max independent set (ore one of them) in the following graph.",
+    MIN_VERTEX_COVER_VERTEX_SELECTION: "Select vertices which make the min vertex cover (ore one of them) in the following graph.",
     COMPLETE_GRAPHS_DRAW: "Draw the following graph: K {}",
     PATH_GRAPHS_DRAW: "Draw the following graph: P {}",
     CYCLE_GRAPHS_DRAW: "Draw the following graph: C {}",
