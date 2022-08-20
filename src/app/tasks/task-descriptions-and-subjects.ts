7513eadd2ee5eb80d156850ea42c574089362743
jakubbedra <jakubbedra@wp.pl>
2022-08-20 11:22:01 +0200

    - Added "Are the given graphs homeomorphic?" task

--- src/app/tasks/task-descriptions-and-subjects.ts
+++ src/app/tasks/task-descriptions-and-subjects.ts
@@ -21,7 +21,8 @@ export class TaskDescriptionsAndSubjects {
     TREE_GRAPHS: "Tree Graphs",
     BIPARTITE_GRAPHS: "Bipartite Graphs",
     PLANAR_GRAPHS: "Planar Graphs",
-    ISOMORPHISM: "Isomorphism"
+    ISOMORPHISM: "Isomorphism",
+    HOMEOMORPHISM: "Homeomorphism"
   };
 
   public static DESCRIPTIONS: Record<string, string> = {
@@ -48,7 +49,8 @@ export class TaskDescriptionsAndSubjects {
     BIPARTITE_GRAPHS_DRAW: "Draw the following graph: K {r}, {s}",
     BIPARTITE_GRAPHS_BOOLEAN: "Is the given graph Bipartite?",
     PLANAR_GRAPHS_BOOLEAN: "Is the given graph Planar?",
-    ISOMORPHISM_BOOLEAN: "Are the given graphs Isomorphic?"
+    ISOMORPHISM_BOOLEAN: "Are the given graphs Isomorphic?",
+    HOMEOMORPHISM_BOOLEAN: "Are the given graphs Homeomorphic?"
   };
 
 }
